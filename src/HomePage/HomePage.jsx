778054385bf8048686062a0cfd6d83b9dcfdc357
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-02-23 19:13:29 +0100

    re-design the landingPage

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -3,11 +3,11 @@ import styles from "./HomePage.module.css";
 function HomePage() {
   return (
     <div className={styles.container}>
-      <div className={styles.imageSection}></div>
-      <div className={styles.colorSection}>
-        <h1 className={styles.motto}>A cup as unique as your taste</h1>
-        <p className={styles.link}>Discover your favorite</p>
+      <div className={styles.imageSection}>
+        <img src="/landingPage.png" alt="landing page" />
       </div>
+      <h1 className={styles.motto}>A cup as unique as your taste</h1>
+      <p className={styles.link}>Discover your favorite</p>
     </div>
   );
 }
