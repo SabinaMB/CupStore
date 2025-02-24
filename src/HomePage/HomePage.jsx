20e3dc3677f9772ded33ba6ac60af14e81443bd9
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-02-24 11:09:26 +0100

    responsive mediun-large screen

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -3,9 +3,8 @@ import styles from "./HomePage.module.css";
 function HomePage() {
   return (
     <div className={styles.container}>
-      <div className={styles.imageSection}>
-        <img src="/landingPage.png" alt="landing page" />
-      </div>
+      <img className={styles.image} src="/landingPage.png" alt="landing page" />
+
       <h1 className={styles.motto}>A cup as unique as your taste</h1>
       <p className={styles.link}>Discover your favorite</p>
     </div>
