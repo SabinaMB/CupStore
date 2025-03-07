2bb2dca8de1d713265e2d16285fb3ed3312391a3
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-03-07 10:38:09 +0100

    fixing styling for ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -6,22 +6,27 @@ const ProductCard = ({ product, view, children }) => {
   const { image, name, price } = product;
   return (
     <div className={style.productCard}>
-      {view === "catalog" && !product.inCart && (
-        <div className={style.cardBtns}>
-          <CardButtons product={product} />
-        </div>
-      )}
+      <div className={style.cardHeader}>
+        {view === "catalog" && !product.inCart && (
+          <div className={style.cardBtns}>
+            <CardButtons product={product} />
+          </div>
+        )}
+        {view === "catalog" && product.inCart && (
+          <p className={style.inCartText}>In Cart</p>
+        )}
+      </div>
 
-      {view === "catalog" && product.inCart && (
-        <p className={style.inCartText}>In Cart</p>
-      )}
-
-      <div className={style.product}>
+      <div className={style.imageWrapper}>
         <img src={image} alt={name} />
+      </div>
+
+      <div className={style.cardFooter}>
         <h3>{name}</h3>
         <p>{price} €</p>
-        {view === "cart" && children}
       </div>
+
+      {view === "cart" && children}
     </div>
   );
 };
